680f1e0a2724252122bcffd10c89c4b6e0616b1b
Adam Lisowski <48516273+adamfoxman@users.noreply.github.com>
2022-10-02 19:01:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "version": os.getenv("SWEARINOX_API_VERSION"),
+        "motto": "Everything counts in large amounts",
+        }
