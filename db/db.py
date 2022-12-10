db2d716ab4d93ee28060a406622d4a1f2d5cc161
Adam Lisowski <48516273+adamfoxman@users.noreply.github.com>
2022-12-10 13:21:11 +0100

    database code reformat

--- db/db.py
+++ db/db.py
@@ -7,15 +7,11 @@ class Database:
     def __init__(self):
         self.guilds = deta.Base("guilds")
         self.users = deta.Base("users")
+        
+    # ========== GUILD METHODS ==========
 
     def get_guild(self, guild_id):
         return self.guilds.get(guild_id)
-
-    def get_user(self, user_id):
-        return self.users.get(user_id)
-
-    def get_words(self, guild_id):
-        return self.guilds.get(guild_id)["words"]
 
     def create_guild(self, guild_id, guild_name)
         self.guilds.put({
@@ -23,6 +19,14 @@ class Database:
             "name": guild_name,
             "words": []
         })
+    
+    def delete_guild(self, guild_id):
+        self.guilds.delete(guild_id)
+    
+    # ========== USER METHODS ==========
+    
+    def get_user(self, user_id):
+        return self.users.get(user_id)
 
     def create_user(self, guild_id, member_id, member_name):
         new_user = {
@@ -33,6 +37,19 @@ class Database:
         words = self.get_words(guild_id)
         new_user["words"] = words
         self.users.put(new_user)
+    
+    def delete_user(self, user_id):
+        self.users.delete(user_id)
+
+    def __update_all_users_in_guild(self, update_dict, guild_id):
+        users = self.users.fetch({"guild_id": guild_id})
+        for user in users:
+            self.users.update(update_dict, user["key"])
+
+    # ========== WORD METHODS ==========
+
+    def get_words(self, guild_id):
+        return self.guilds.get(guild_id)["words"]
 
     def add_word(self, guild_id, word, stem):
         word_dict = {
@@ -41,16 +58,11 @@ class Database:
             "count": 0
         }
         self.guilds.update({
-            "words": self.guilds.utils.append(word_dict)
+            "words": self.guilds.util.append(word_dict)
         }, guild_id)
         self.__update_all_users_in_guild({
-            "words": self.users.utils.append(word_dict)
+            "words": self.users.util.append(word_dict)
         }, guild_id)
-
-    def __update_all_users_in_guild(self, update_dict, guild_id):
-        users = self.users.fetch({"guild_id": guild_id})
-        for user in users:
-            self.users.update(update_dict, user["key"])
 
     def increment_word(self, guild_id, member_id, word, count):
         user = self.users.fetch({"guild_id": guild_id, "member_id": member_id})
